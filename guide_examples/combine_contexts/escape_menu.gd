d9edc06734314e52c86208f4d8c009932058a2af
ShadowCommander <10494922+ShadowCommander@users.noreply.github.com>
2026-02-13 20:32:15 -0800

    Test escape menu does not disable movement

--- guide_examples/combine_contexts/escape_menu.gd
+++ guide_examples/combine_contexts/escape_menu.gd
@@ -6,7 +6,7 @@ extends Control
 
 var _previous_mapping_contexts: Array[GUIDEMappingContext] = []
 
-# Called when the node enters the scene tree for the first time.
+
 func _ready() -> void:
 	hide()
 	open_menu_action.triggered.connect(open_menu)
@@ -14,22 +14,13 @@ func _ready() -> void:
 
 func open_menu() -> void:
 	show()
-	_previous_mapping_contexts = GUIDE.get_enabled_mapping_contexts()
-	_enable_menu_mapping_contexts(menu_contexts)
+	for context in menu_contexts:
+		GUIDE.enable_mapping_context(context)
 
 func resume_game() -> void:
 	hide()
-	_enable_menu_mapping_contexts(_previous_mapping_contexts)
-	
-func _enable_menu_mapping_contexts(contexts: Array[GUIDEMappingContext]) -> void:
-	var first: bool = true
-	
-	for context in contexts:
-		if first:
-			GUIDE.enable_mapping_context(context, true)
-			first = false
-		else:
-			GUIDE.enable_mapping_context(context)
+	for context in menu_contexts:
+		GUIDE.disable_mapping_context(context)
 
 
 func _on_button_quit_pressed() -> void:
